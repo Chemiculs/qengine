76b9a6b5e018fc88f31efda00a178db14f16fc3c
0xHAX0R <lanegraham3@gmail.com>
2023-08-01 15:44:54 -0700

    updated vs project to newest version

---
+++ vs/qengine/qengine/engine/hashtypes/qhash_t.cpp
@@ -0,0 +1,13 @@
+#include "qhash_t.hpp"
+
+
+namespace qengine {
+
+	namespace qhash_t {
+
+		qcallback::qmem_exception_rogue_c qhash_t::violation_callback;
+
+		bool qhash_t::is_init = false;
+	}
+
+}
---
+++ vs/qengine/qengine/engine/polyhashtypes/qenc_h_t.cpp
@@ -0,0 +1,9 @@
+#include "qenc_h_t.hpp"
+
+namespace qengine {
+
+	namespace qenc_h_t {
+
+		bool qenc_h_t::is_init = false;
+	}
+}
