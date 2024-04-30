4beacf0760cb85908805d43ac1549cbd7c9a98db
0xHAX0R <lanegraham3@gmail.com>
2024-04-30 07:28:11 -0700

    Update qdef.hpp

--- vs/qengine/qengine/qbase/qdef.hpp
+++ vs/qengine/qengine/qbase/qdef.hpp
@@ -38,7 +38,7 @@
 
 #pragma region Method Attributes
 
-#define __singleton __declspec(noinline)	//	we only want a static instance of the declared fn, not instanced copies into caller functions
+#define __singleton __declspec(noinline)	//	we only want a static instance of the declared fn per object, not instanced copies into caller functions
 
 #define __nothrow __declspec(nothrow)	//	explicit instruction to compiler to disable any SEH related code generation (this does happen implicitly anyways, yes)
 
