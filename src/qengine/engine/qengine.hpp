239babe4e1df2ae6c0cbf5641d3b22ae98516171
0xHAX0R <lanegraham3@gmail.com>
2024-05-05 21:52:29 -0700

    Add files via upload
    Signed-off-by: 0xHAX0R <lanegraham3@gmail.com>

---
+++ src/qengine/engine/qengine.hpp
@@ -0,0 +1,22 @@
+#ifndef QENGINE_H
+#define QENGINE_H
+
+#pragma region Imports
+
+#pragma region qengine
+
+#include "polytypes/qtype_enc.hpp"
+#include  "hashtypes/qtype_hash.hpp"
+#include  "polyhashtypes/qtype_enchash.hpp"
+#include "../qhook/qhook.hpp"
+#include  "../qutil/qcritical.hpp"
+#include "../qimport/qimport.hpp"
+#include "../qmorph/qdisasm.hpp"
+#include "../qbase/qcallback.hpp"
+#include "../qbase/qexcept.hpp"
+
+#pragma endregion
+
+#pragma endregion
+
+#endif
