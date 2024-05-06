2f7af0eb51a0eb6578cc44311aad5bf44fa10fb1
0xHAX0R <lanegraham3@gmail.com>
2024-05-05 21:13:50 -0700

    Add files via upload
    Signed-off-by: 0xHAX0R <lanegraham3@gmail.com>

---
+++ src/qengine/qbase/qcallback.hpp
@@ -0,0 +1,20 @@
+#ifndef QCALLBACK_H
+#define QCALLBACK_H
+
+#include "qexcept.hpp"
+
+namespace qengine {
+
+	namespace qcallback {
+
+#pragma region Callback FN Prototypes
+
+		typedef void(__regcall* qmem_exception_rogue_c)(qexcept::q_rogueaccess violation, void* data);
+
+		typedef void(__regcall* qmem_except_hook)(qexcept::q_fn_alteration violation);
+
+#pragma endregion
+	}
+}
+
+#endif
